cf9d09abacbbbda8b14cf4d1521714a1721678d4
Benned Hedegaard <benned@Benneds-MacBook-Pro.local>
2019-10-26 01:02:55 -0400

    Backup for night

--- audio_input.py
+++ audio_input.py
@@ -6,28 +6,38 @@ Last revised: 10/25/2019 by Benned H
 # wavio lets us convert NumPy <--> .wav and back
 
 import sounddevice as sd
+import numpy as np
 from scipy.io.wavfile import write
 from time import sleep
+import matplotlib.pyplot as plt
 
-def countdown(n):
+def countdown(n,scale=1):
 	# Prints a countdown to start recording.
-	for i in range(n,0,-1):
+	for i in range(n*scale,0,-1):
 		print(i)
-		sleep(1)
+		sleep(1/scale)
+
+def parsePitch(arr,steps):
+	pass
 
 def main():
 	hz = 44100  # Sample rate
-	seconds = 1  # Duration of recording
+	seconds = 1 # Duration of recording
+	timesteps = int(seconds * hz)
 
-	#print("Recording in...")
-	#countdown(seconds)
-	record = sd.rec(int(seconds * hz), samplerate=hz, channels=1)
+	print("Recording in...")
+	countdown(1,10)
+	record = sd.rec(timesteps, samplerate=hz, channels=1)
 	print("Recording for",seconds,"seconds...")
-	#countdown(seconds)
+	countdown(seconds)
 	
 	sd.wait()  # Wait until recording is finished
 	print("Type:",type(record),"Shape",record.shape,"Max",max(record))
 
+	plt.plot(np.fft.fft(record))
+	plt.show()
+
+	#np.savetxt("recording.txt", record)
 	#write('output.wav', hz, record)  # Save as WAV file
 	#print("output.wav saved.")
 
---
+++ fft.py
@@ -0,0 +1,23 @@
+"""
+Trying to create a fourier transform example using Numpy.
+Benned Hedegaard - 10/26/2019
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+hz = 44100
+seconds = 1 # In seconds
+timesteps = seconds * hz
+
+two_pi = 2*np.pi
+x = np.linspace(0.0, seconds, timesteps)
+y = np.sin(10000*two_pi*x) + np.sin(5000*two_pi*x)
+#y = np.sin(16000*two_pi*x) - np.sin(8000*two_pi*x) + np.sin(4000*two_pi*x) - np.sin(2000*two_pi*x) + np.sin(1000*two_pi*x) - np.sin(500*two_pi*x) + np.sin(250*two_pi*x) - np.sin(125*two_pi*x)
+#y = np.sin(16000*two_pi*x) + np.sin(8000*two_pi*x) + np.sin(4000*two_pi*x) + np.sin(2000*two_pi*x) + np.sin(1000*two_pi*x) + np.sin(500*two_pi*x) + np.sin(250*two_pi*x) + np.sin(125*two_pi*x)
+
+yf = np.fft.fft(y) # Fourier transform y
+xf = range(len(yf))
+
+plt.plot(xf,yf)
+plt.show()
