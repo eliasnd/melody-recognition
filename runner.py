0f53011abd3b21f5c12465feda52aa9457928af7
Benned Hedegaard <benned@Benneds-MacBook-Pro.local>
2019-10-26 22:11:00 -0400

    merge

--- runner.py
+++ runner.py
@@ -17,8 +17,8 @@ def main():
 	birthday = ['C', 'D', 'C', 'F', 'E', 'E', 'C', 'D', 'C', 'G', 'F', 'F', 'C', 'C', 'A', 'F', 'E', 'D', 'A#', 'A', 'F', 'G', 'F', 'F', 'F']
 
 	# Africa -- Toto -- Eighth note pulse
-	africa = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'G', 'G', 'C', 'C', 'D', 'C', 'A', 'C', 'A', 'D', 'D', 'C', 'A', 'C', 
-			  'D', 'C', 'A', 'G', 'A', 'D', 'C']
+	africa = [	'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'G', 'G', 'C', 'C', 'D',
+				'C', 'A', 'C', 'A', 'D', 'D', 'C', 'A', 'C', 'D', 'C', 'A', 'G', 'A', 'D', 'C']
 
 	# US National Anthem -- Eighth note pulse
 	anthem = ['G', 'E', 'C', 'C', 'E', 'E', 'G', 'G', 'C', 'C', 'C', 'C', 'E', 'D', 'C', 'C', 'E', 'E', 'F#', 'F#', 
