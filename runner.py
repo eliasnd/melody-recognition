84e0f45d0099f94147bee121fc9b6ceb4c5d769e
eliasnd <elias@ndcoding.com>
2019-10-26 20:24:23 -0400

    Wrote melodies, revamped piano roll

--- runner.py
+++ runner.py
@@ -2,13 +2,21 @@ from audio_input import recordMelody
 from piano_roll import *
 
 def main():
-	melody = recordMelody(3)
-	print(reduce(melody))
 
-	odetojoy = ['E', 'E', 'F', 'G']
-	print(reduce(odetojoy))
+	# Repertoire
 
-	print(similarity(reduce(melody), reduce(odetojoy)))
+	# Happy Birthday -- Quarter note pulse
+	birthday = ['C', 'D', 'C', 'F', 'E', 'E', 'C', 'D', 'C', 'G', 'F', 'F', 'C', 'C', 'A', 'F', 'E', 'D', 'A#', 'A', 'F', 'G', 'F', 'F', 'F']
+
+	# Africa -- Toto -- Eighth note pulse
+	africa = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'G', 'G', 'C', 'C', 'D', 'C', 'A', 'C', 'A', 'D', 'D', 'C', 'A', 'C', 
+			  'D', 'C', 'A', 'G', 'A', 'D', 'C']
+
+	# US National Anthem -- Eighth note pulse
+	anthem = ['G', 'E', 'C', 'C', 'E', 'E', 'G', 'G', 'C', 'C', 'C', 'C', 'E', 'D', 'C', 'C', 'E', 'E', 'F#', 'F#', 
+			  'G', 'G', 'G', 'G', 'G', 'G', 'E', 'E', 'E', 'D', 'C', 'C', 'B', 'B', 'B', 'B', 'A', 'B', 'C', 'C', 'C', 'C', 
+			  'G', 'G', 'E', 'E', 'C', 'C', 'C', 'C']
+
 
 if __name__ =="__main__":
 	main()
